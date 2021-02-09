842d7efa138d1cf76701e46739a36cc890123878
Daniel Calviño Sánchez <danxuliu@gmail.com>
2021-02-09 02:12:15 +0100

    Use peer data to hide listeners if participant data is not available
    Guests can not fetch the full participant list, so the participant store
    is empty when the current user is a guest. However, guests can fetch the
    peer data, so now that data is used when the participant data is not
    available to find out if another participant has publishing permissions
    or not and hide her if needed in the call view.
    
    The peer data is extended with "publishing permissions" and it is also
    updated on "participantListChanged" events emitted by the signaling, as
    until now it was updated only if it was required by a Video element but
    it was not available.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/components/CallView/CallView.vue
+++ src/components/CallView/CallView.vue
@@ -229,7 +229,12 @@ export default {
 
 				const participantIndex = this.$store.getters.getParticipantIndex(this.token, { sessionId: nextcloudSessionId })
 				if (participantIndex === -1) {
-					return false
+					const peerData = this.$store.getters.getPeer(this.token, nextcloudSessionId)
+					if (!peerData) {
+						return false
+					}
+
+					return peerData.publishingPermissions !== PARTICIPANT.PUBLISHING_PERMISSIONS.NONE
 				}
 
 				const participant = this.$store.getters.getParticipant(this.token, participantIndex)
@@ -440,11 +445,14 @@ export default {
 	},
 	mounted() {
 		EventBus.$on('refreshPeerList', this.debounceFetchPeers)
+		EventBus.$on('Signaling::participantListChanged', this.debounceFetchPeers)
+		this.debounceFetchPeers()
 
 		callParticipantCollection.on('remove', this._lowerHandWhenParticipantLeaves)
 	},
 	beforeDestroy() {
 		EventBus.$off('refreshPeerList', this.debounceFetchPeers)
+		EventBus.$off('Signaling::participantListChanged', this.debounceFetchPeers)
 
 		callParticipantCollection.off('remove', this._lowerHandWhenParticipantLeaves)
 	},
